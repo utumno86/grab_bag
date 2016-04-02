a5698160f738af2052247431e54e2f3163193999
Michael E. Nash <mnash@investcloud.com>
2016-04-02 15:30:14 -0400

    added an if statement

--- brew-cask-update.groovy
+++ brew-cask-update.groovy
@@ -4,8 +4,7 @@ programList.each { program ->
   println "$program"
   // println "brew cask install --force $program".execute().text
   def versionList = "ls -trh /opt/homebrew-cask/Caskroom/$program".execute().text.split("\n")
-  println versionList.size()
-  versionList.each { version ->
-    println "$version"
+  if (versionList.size() > 1) {
+    println(versionList[0])
   }
 }
