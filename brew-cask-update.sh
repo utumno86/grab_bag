e5b58e397181f82d6dc99c017c1e9ce26695d476
Michael E. Nash <mnash@investcloud.com>
2016-04-02 13:54:48 -0400

    uncommented the functional part of the script

--- brew-cask-update.sh
+++ brew-cask-update.sh
@@ -3,7 +3,7 @@ for program in `brew cask list`
 
 do
   echo $program
-  # brew cask install --force "$program"
+  brew cask install --force "$program"
   for version in `ls -trh /opt/homebrew-cask/Caskroom/$program`
 
   do
