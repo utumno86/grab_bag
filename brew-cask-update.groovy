4cc2ec5e79b0406f910ca5ee5a307559cc94f9ad
Michael E. Nash <mnash@investcloud.com>
2016-04-02 15:26:55 -0400

    printed a diagnostic version

--- brew-cask-update.groovy
+++ brew-cask-update.groovy
@@ -2,5 +2,10 @@ def programList = "brew cask list".execute().text.split("\n")
 
 programList.each { program ->
   println "$program"
-  println "brew cask install --force $program".execute().text
+  // println "brew cask install --force $program".execute().text
+  def versionList = "ls -trh /opt/homebrew-cask/Caskroom/$program".execute().text.split("\n")
+  println versionList.size()
+  versionList.each { version ->
+    println "$version"
+  }
 }
