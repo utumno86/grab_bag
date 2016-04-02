1e657f627a5b53c3825bbb2f31287eafc5e7690c
Michael E. Nash <mnash@investcloud.com>
2016-04-02 16:10:37 -0400

    completed the script

--- brew-cask-update.groovy
+++ brew-cask-update.groovy
@@ -2,11 +2,12 @@ def programList = "brew cask list".execute().text.split("\n")
 
 programList.each { program ->
   println "$program"
-  // println "brew cask install --force $program".execute().text
+  println "brew cask install --force $program".execute().text
   def versionList = "ls -trh /opt/homebrew-cask/Caskroom/$program".execute().text.split("\n")
   if (versionList.size() > 1) {
     def removeFolder = { i ->
-      println versionList[i]
+      println "Removing version: ${versionList[i]}"
+      println "rm -rf /opt/homebrew-cask/Caskroom/$program/${versionList[i]}".execute().text
     }
     0.upto(versionList.size() - 2, removeFolder)
   }
