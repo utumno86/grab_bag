607ae32d54a24a25c3d8ce140b52bdff7537eb11
Michael E. Nash <mnash@investcloud.com>
2016-04-02 13:05:50 -0400

    wrote line to list each version of a particular program

--- brew-cask-update.sh
+++ brew-cask-update.sh
@@ -3,5 +3,6 @@ for program in `brew cask list`
 
 do
   echo $program
-  brew cask install --force "$program"
+  # brew cask install --force "$program"
+  echo `ls -trh /opt/homebrew-cask/Caskroom/$program`
 done
