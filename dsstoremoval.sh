b9fc7d6980294e2022c5f0709ce15718306a8ab4
Michael E. Nash <mnash@investcloud.com>
2016-06-27 17:36:45 -0400

    dsstoremoval

---
+++ dsstoremoval.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for USER_TEMPLATE in "/System/Library/User Template"/* do /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/.GlobalPreferences DSDontWriteNetworkStores -bool TRUE done
