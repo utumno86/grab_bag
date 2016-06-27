b56de611a639ca1afbbf94ad55d919af4037e66f
Michael E. Nash <mnash@investcloud.com>
2016-06-27 17:42:47 -0400

    updated the formatting

--- dsstoremoval.sh
+++ dsstoremoval.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-for USER_TEMPLATE in "/System/Library/User Template"/* do /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/.GlobalPreferences DSDontWriteNetworkStores -bool TRUE done
+ #!/bin/bash
+for USER_TEMPLATE in "/System/Library/User Template"/*
+do
+  /usr/bin/defaults write "${USER_TEMPLATE}"/Library/Preferences/.GlobalPreferences DSDontWriteNetworkStores -bool TRUE
+done
