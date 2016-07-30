7aee896ecb05683e03e044ddfc750bd13c19ffbd
mnash <utumno86@gmail.com>
2016-07-29 23:06:02 -0400

    updated the brew update script with the correct file paths

--- brew-cask-update.groovy
+++ brew-cask-update.groovy
@@ -3,11 +3,11 @@ def programList = "brew cask list".execute().text.split("\n")
 programList.each { program ->
   println "$program"
   println "brew cask install --force $program".execute().text
-  def versionList = "ls -trh /opt/homebrew-cask/Caskroom/$program".execute().text.split("\n")
+  def versionList = "ls -trh /usr/local/Caskroom/$program".execute().text.split("\n")
   if (versionList.size() > 1) {
     def removeFolder = { i ->
       println "Removing version: ${versionList[i]}"
-      println "rm -rf /opt/homebrew-cask/Caskroom/$program/${versionList[i]}".execute().text
+      println "rm -rf /usr/local/Caskroom/$program/${versionList[i]}".execute().text
     }
     0.upto(versionList.size() - 2, removeFolder)
   }
