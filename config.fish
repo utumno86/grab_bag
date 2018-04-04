ddfd6c2cb838fca903605a58da8f72c909a9b787
Michael Nash <michael@spatialnetworks.com>
2018-04-04 17:46:11 -0400

    updated my fish config

--- config.fish
+++ config.fish
@@ -1,30 +1,65 @@
 set -g -x PATH /usr/local/bin $PATH
-set -g -x fish_greeting 'Good Morning Michael! ALL YOUR BASE ARE BELONG TO US!!!!'
+set -g -x fish_greeting 'Release every zig for great justice!'
+cal
+date
 
-export BUNDLER_EDITOR=atom
+# Ruby
+export BUNDLER_EDITOR=code
+status --is-interactive; and source (rbenv init -|psub)
+
+# Go
+export GOPATH=/Users/michael/Developer/GoWorkspace/
+set -g -x PATH $GOPATH/bin $PATH
+
+# Rust
+set -g -x PATH $HOME/.cargo/bin $PATH
+
+#Groovy
+export GROOVY_HOME=/usr/local/opt/groovy/libexec
+
+#Pokemon Terminal
+set -g -x PATH $HOME/.Pokemon-Terminal $PATH
+pokemon -r kanto --dark
+
+#Tiny Care Terminal
+export TTC_BOTS=magicrealismbot,tinycarebot,selfcare_bot
+export TTC_WEATHER=Tampa
+export TTC_CELSIUS=false
+export TTC_REPOS=~/Developer
+export TTC_APIKEYS=true
+export TTC_UPDATE_INTERVAL=20
+export TTC_SAY_BOX=parrot
+export TTC_REPOS_DEPTH=5
+
+#Twitter
+export CONSUMER_KEY=
+export CONSUMER_SECRET=
+export ACCESS_TOKEN=
+export ACCESS_TOKEN_SECRET=
+
+# Github
+export HOMEBREW_GITHUB_API_TOKEN=
 
 alias att="atom ."
 alias gu="gem update"
 alias gemc="gem cleanup"
-alias nu="npm update"
-alias plo="pip list --outdated"
-alias piu="pip install --upgrade"
-alias p3lo="pip3 list --outdated"
-alias p3iu="pip3 install --upgrade"
+alias nu="npm update -g"
+alias plo="pip3 list --outdated"
+alias piu="pip3 install --upgrade"
 alias au="apm update"
 alias bu="brew update"
 alias bo="brew outdated"
 alias bup="brew upgrade -all"
 alias bc="brew cleanup"
 alias ll="ls -lathr"
-alias exc="cd ~/Coding/exercism_exercises/ruby"
-alias profile="cd ~/Coding/profile"
-alias ef="exercism fetch"
-alias es="exercism submit"
-alias el="exercism list"
 alias pgstart="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
 alias pgstop="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log stop"
 alias fs="foreman start"
-alias bcup="groovy $ETL_ROOT_DIRECTORY/UsefulCrap/brew-cask-update.groovy"
+alias bcup="groovy /Users/michael/Developer/JunkBin/Scripts/brew-cask-update.groovy"
 alias log="git log --graph --pretty=format:'%Cred%H%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'"
 alias del="rm -rf"
+alias dcu="docker-compose up"
+alias dcd="docker-compose down"
+alias ctt="code ."
+alias gospace="cd ~/Developer/GoWorkspace/src/github.com/utumno86"
+alias ipaddress="ifconfig | grep 'inet ' | grep -Fv 127.0.0.1 | awk '{print $2}'"
